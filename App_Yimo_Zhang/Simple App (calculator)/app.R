dc183def52a44cbb2d23d11c8f382590a37e6543
Yimo Zhang <yimo_zhang@brown.edu>
2017-12-17 00:11:17 -0500

    P value calculator.

---
+++ App_Yimo_Zhang/Simple App (calculator)/app.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(shinyjs)
+
+
+ui <- fluidPage(
+  useShinyjs(),
+  headerPanel("P-value Calculator"),
+  br(),
+  hr(),
+  sidebarLayout(
+    sidebarPanel(
+      div(id = "cal",
+      numericInput(inputId = "z", label = "Z-value", value = 0),
+      sliderInput(inputId = "mean", label = "Mean", value = 0, min = -20, max = 20),
+      sliderInput(inputId = "variance", label = "Variance", value = 1, min = 1, max = 30),
+      actionButton(inputId = "go", label = "Calculate")),
+      br(),
+      actionButton(inputId = "reset", "Reset")
+    ),
+    mainPanel(
+    verbatimTextOutput("p")
+  )
+  )
+)
+
+server <- function(input, output){
+  
+  output$p = renderText({
+    input$go
+    isolate(paste("P-value = ", -pnorm(input$z, mean = input$mean, sd = sqrt(input$variance)), sep = ""))
+  })
+  
+  observeEvent(input$reset, reset("cal"))
+}
+ 
+shinyApp(ui = ui, server = server)
